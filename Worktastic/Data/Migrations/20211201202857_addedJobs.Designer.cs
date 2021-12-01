cc2703e1cd4d37dd73010a8ffdb2d36139eeb775
LeDaniii <danielmatzer@gmail.com>
2021-12-01 22:30:23 +0100

    update database

--- Worktastic/Data/Migrations/20211201202857_addedJobs.Designer.cs
+++ Worktastic/Data/Migrations/20211201202857_addedJobs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Worktastic.Data;
 
@@ -11,9 +12,10 @@ using Worktastic.Data;
 namespace Worktastic.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211201202857_addedJobs")]
+    partial class addedJobs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
